56c3964558eea489bd9a2d2ecdf8fc5e148b420f
Deekshant@gmail.com <d33kshant@gmail.com>
2023-08-13 14:29:11 +0530

    Add browser router for app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import PlaylistPage from './pages/PlaylistPage.tsx'
+import HomePage from './pages/HomePage.tsx'
 import './index.css'
+
+function withApp(elem: any) {
+	return (
+		<App>
+			{elem}
+		</App>
+	)
+}
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />
+	},
+	{
+		path: "/playlist/:id",
+		element: <PlaylistPage />
+	}
+].map(page => ({ ...page, element: withApp(page.element) })))
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>,
 )
